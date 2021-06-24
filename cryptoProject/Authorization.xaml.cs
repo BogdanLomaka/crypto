84c6e930801a1f7c3a56611cc3929925934a7282
BogdanLomaka <63854468+BogdanLomaka@users.noreply.github.com>
2021-06-24 13:35:22 +0300

    Add files via upload

---
+++ cryptoProject/Authorization.xaml.cs
@@ -0,0 +1,131 @@
+﻿using RestSharp;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace cryptoProject {
+    /// <summary>
+    /// Логика взаимодействия для Authorization.xaml
+    /// </summary>
+    public partial class Authorization : Window {
+        public static string GetHash(string input) {
+            // Use input string to calculate MD5 hash
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create()) {
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                // Convert the byte array to hexadecimal string
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++) {
+                    sb.Append(hashBytes[i].ToString("X2"));
+                }
+                return sb.ToString();
+            }
+        }
+
+        public async void authorize() {
+            var login = UsernameTextBox.Text;
+            var password = GetHash(UsernameTextBox2.Password);
+
+            var client = new RestClient($"http://82.146.61.12:3001/api/login?login={login}&password={password}");
+            client.Timeout = -1;
+
+            var request = new RestRequest(Method.GET);
+            request.OnBeforeDeserialization = resp => { resp.ContentType = "application/json"; };
+            var response = await client.ExecuteAsync(request);
+
+            if (bool.Parse(response.Content)) {
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+                this.Close();
+            } else
+                ErrorLabel.Visibility = Visibility.Visible;
+        }
+        public async void register() {
+            var login = UsernameTextBox.Text;
+            var password = GetHash(UsernameTextBox2.Password);
+
+            var client = new RestClient($"http://82.146.61.12:3001/api/register?login={login}&password={password}");
+            client.Timeout = -1;
+
+            var request = new RestRequest(Method.GET);
+            request.OnBeforeDeserialization = resp => { resp.ContentType = "application/json"; };
+            var response = await client.ExecuteAsync(request);
+
+            if (bool.Parse(response.Content)) {
+                GoodLabel.Visibility = Visibility.Visible;
+            } else ErrorCreateLabel.Visibility = Visibility.Visible;
+        }
+        public Authorization() {
+            InitializeComponent();
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e) {
+            UsernamePlaceholder.Visibility = Visibility.Collapsed;
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e) {
+            if (sender is TextBox) {
+                var textbox = sender as TextBox;
+                if (string.IsNullOrEmpty(textbox.Text)) {
+                    UsernamePlaceholder.Visibility = Visibility.Visible;
+                }
+            }
+        }
+
+        private void TextBox_GotFocus2(object sender, RoutedEventArgs e) {
+            UsernamePlaceholder2.Visibility = Visibility.Collapsed;
+        }
+
+        private void TextBox_LostFocus2(object sender, RoutedEventArgs e) {
+            if (sender is TextBox) {
+                var textbox = sender as TextBox;
+                if (string.IsNullOrEmpty(textbox.Text)) {
+                    UsernamePlaceholder2.Visibility = Visibility.Visible;
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            authorize();
+        }
+
+        private void Button_Click1(object sender, RoutedEventArgs e) {
+            register();
+        }
+
+        private void UsernameTextBox2_KeyDown(object sender, System.Windows.Input.KeyEventArgs e) {
+            if (e.Key == System.Windows.Input.Key.Enter) {
+                if (btmLoginButton.Visibility == Visibility.Hidden)
+                    authorize();
+                else
+                    register();
+            } 
+        }
+
+        private void UsernameTextBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e) {
+            if (e.Key == System.Windows.Input.Key.Enter) {
+                if (btmLoginButton.Visibility == Visibility.Hidden)
+                    authorize();
+                else
+                    register();
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e) {
+            loginButton.Visibility = Visibility.Hidden;
+            registerButton.Visibility = Visibility.Visible;
+            btmRegisterButton.Visibility = Visibility.Hidden;
+            btmLoginButton.Visibility = Visibility.Visible;
+            ErrorLabel.Visibility = Visibility.Hidden;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e) {
+            loginButton.Visibility = Visibility.Visible;
+            registerButton.Visibility = Visibility.Hidden;
+            btmRegisterButton.Visibility = Visibility.Visible;
+            btmLoginButton.Visibility = Visibility.Hidden;
+            ErrorCreateLabel.Visibility = Visibility.Hidden;
+        }
+    }
+}
---
+++ cryptoProject/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Newtonsoft;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+using System.Globalization;
+
+namespace cryptoProject {
+    public partial class MainWindow : Window {
+        class crypto {
+            public string price { get; set; }
+        }
+
+        public async void getCourse() {
+            var privateKey = "f40ada9ce4f2f5769bdcf871d15238a3f06f9122";
+            var ids = "BTC,ETH,XRP";
+            var index = Combo.SelectedIndex;
+            var convert = "";
+
+            if (index == 0)
+                convert = "USD";
+            else if (index == 1)
+                convert = "RUB";
+            else if (index == 2)
+                convert = "UAH";
+
+            var client = new RestClient($"https://api.nomics.com/v1/currencies/ticker?key={privateKey}&ids={ids}&convert={convert}");
+            client.Timeout = -1;
+
+            var request = new RestRequest(Method.GET);
+            request.OnBeforeDeserialization = resp => { resp.ContentType = "application/json"; };
+            var response = await client.ExecuteAsync(request);
+
+            var data = JsonConvert.DeserializeObject<List<crypto>>(response.Content);
+
+            btc_price.Content = (Math.Round(double.Parse(UsernameTextBox_Main.Text, CultureInfo.InvariantCulture) / double.Parse(data[0].price, CultureInfo.InvariantCulture) * 10000) / 10000).ToString();
+            eth_price.Content = (Math.Round(double.Parse(UsernameTextBox_Main.Text, CultureInfo.InvariantCulture) / double.Parse(data[1].price, CultureInfo.InvariantCulture) * 10000) / 10000).ToString();
+            xpr_price.Content = (Math.Round(double.Parse(UsernameTextBox_Main.Text, CultureInfo.InvariantCulture) / double.Parse(data[2].price, CultureInfo.InvariantCulture) * 10000) / 10000).ToString();
+        }
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            getCourse();
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e) {
+            UsernamePlaceholder_Main.Visibility = Visibility.Collapsed;
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e) {
+            if (sender is TextBox) {
+                var textbox = sender as TextBox;
+                if (string.IsNullOrEmpty(textbox.Text)) {
+                    UsernamePlaceholder_Main.Visibility = Visibility.Visible;
+                }
+            }
+        }
+
+        private void UsernameTextBox_Main_KeyDown(object sender, KeyEventArgs e) {
+            if (e.Key == System.Windows.Input.Key.Enter)
+                getCourse();
+        }
+    }
+}
